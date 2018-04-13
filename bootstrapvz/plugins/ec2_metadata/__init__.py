b6bdf473f8b571ecf6963209919b55884386d7b6
Veli-Matti Lintu <veli-matti.lintu@nosto.com>
2018-04-13 15:29:12 +0300

    Collect also information about installed gems and pips.

--- bootstrapvz/plugins/ec2_metadata/__init__.py
+++ bootstrapvz/plugins/ec2_metadata/__init__.py
@@ -5,5 +5,8 @@ def validate_manifest(data, validator, error):
 
 def resolve_tasks(taskset, manifest):
     import tasks
+    taskset.add(tasks.AddPackages)
     taskset.add(tasks.QueryPackages)
+    taskset.add(tasks.QueryGems)
+    taskset.add(tasks.QueryPips)
     taskset.add(tasks.WriteAMIMetadata)
